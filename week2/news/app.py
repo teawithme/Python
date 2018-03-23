dcc12ec1ec622e5dbe20bc795c86a68fa7ce75fe
teawithme <tea.shiyanlou@gmail.com>
2018-03-24 05:48:30 +0800

    error correction

--- week2/news/app.py
+++ week2/news/app.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import os, glob
+import json
 from flask import Flask, render_template, abort
 
 app = Flask(__name__)
@@ -10,25 +11,29 @@ app.config['TEMPLATES_AUTO_RELAOD'] = True
 titles = []
 contents = []
 ncdict = {}
-files = glob.glob('~/Documents/python/shiyanlou/week2/files/*.json')
+files = glob.glob('/home/shiyanlou/files/*.json')
+#print(files)
 for f in files:
-    base = os.path.basename('f')
+    base = os.path.basename(f)
+    #print(base)
     with open(f, 'r') as file:
-        raw = file.read()
-        titles.append(raw['title'])
+        raw = json.loads(file.read())
+        #print(type(raw))
+        titles.append(raw["title"])
         contents.append(raw['content'])
         ncdict[os.path.splitext(base)[0]] = raw['content']
-
+#print(titles)
+#print(ncdict)
 @app.route('/')
 def index():
-    return render_template('index.html',title=title)
+    return render_template('index.html',title=titles)
 
 @app.route('/files/<filename>')
 def file(filename):
-    content = contents.get(filename, 'Invalid')
+    content = ncdict.get(filename, 'Invalid')
     if content == 'Invalid':
         abort(404)
-    return render_template('file.html', content)
+    return render_template('file.html', content=content)
 
 @app.errorhandler(404)
 def not_found(error):
