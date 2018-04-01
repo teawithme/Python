98d5492598478b7d6913b1d9f8165f25312f2397
teawithme <tea.shiyanlou@gmail.com>
2018-04-01 16:15:39 -0600

    added challenge13

---
+++ week3/challenge3_4.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+from datetime import datetime
+
+def open_parser(filename):
+    with open(filename) as logfile:
+        #print(logfile.read())
+        pattern = (r''
+                   r'(\d+.\d+.\d+.\d+)\s-\s-\s'
+                   r'\[(.+)\]\s'
+                   r'"GET\s(.+)\s\w+/.+"\s'
+                   r'(\d+)\s'
+                   r'(\d*)\s'
+                   r'"(.+)"\s'
+                   r'"(.+)"'
+                   )
+        parsers = re.findall(pattern, logfile.read())
+    return parsers
+
+def main():
+    logs = open_parser('nginx.log')
+
+    ipct_dict = {}
+    url404_dict = {}
+    for log in logs:
+        ip = log[0]
+        url = log[2]
+        #print(url)
+        status = log[3]
+        if ipct_dict.get(ip) is None:
+            ipct_dict[ip] = 1
+        else:
+            ipct_dict[ip] += 1
+        if status == '404':
+            if url404_dict.get(url) is None:
+                url404_dict[url] = 1
+            else:
+                url404_dict[url] += 1
+
+    ipct_list = sorted(ipct_dict.items(), key=lambda x: x[1], reverse=True)
+    ip_dict = dict([(ipct_list[0])])
+    #print(ip_dict)
+    url_list = sorted(url404_dict.items(), key=lambda x: x[1], reverse=True)
+    url_dict = dict([(url_list[0])])
+    #print(url404_dict)
+
+    return ip_dict, url_dict
+
+if __name__ == '__main__':
+    ip_dict, url_dict = main()
+    print(ip_dict, url_dict)
+    #main()
