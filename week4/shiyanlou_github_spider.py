3419349e210cbe0e98e7599dd63a90417b4b4d0e
teawithme <tea.shiyanlou@gmail.com>
2018-04-06 11:44:28 +0800

    added challenge15

--- week4/shiyanlou_github_spider.py
+++ week4/shiyanlou_github_spider.py
@@ -5,14 +5,15 @@ import scrapy
 class ShiyanlouGithubSpider(scrapy.Spider):
 
     name = 'github-repositories'
-
-    def start_url(self):
+    @property
+    def start_urls(self):
         url_tmpl = 'https://github.com/shiyanlou?page={}&tab=repositories'
+        print(url_tmpl.format(i) for i in range(1, 5))
         return (url_tmpl.format(i) for i in range(1, 5))
 
     def parse(self, response):
-        for repository in response.css('div.[d-inline-block mb-1']):
+        for repository in response.css('li.col-12'):
             yield{
-                'name': repository.xpath('//h3/a/text()').extract_first(),
-                'updated_time': repository.xpath(//relative-time/@datetime).extract_first()
+                'name': repository.xpath('.//h3/a/text()').re_first('[^\w]*(\w+)'),
+                'updated_time': repository.xpath('.//relative-time/@datetime').extract_first()
         }
